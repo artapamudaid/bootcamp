b127cbce193aca5b1e12ce0331b6e230b30e4652
artapamudaid <pamuda.id@gmail.com>
2021-12-12 21:34:05 +0700

    slicing template

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('landingpage');
+})->name('home');
+
+Route::get('login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('checkout', function () {
+    return view('checkout');
+})->name('checkout');
+
+Route::get('success_checkout', function () {
+    return view('success_checkout');
+})->name('success_checkout');
