d2ca859cc933e282fba92ad742b15ceb26fb28f3
artapamudaid <pamuda.id@gmail.com>
2021-12-16 20:36:34 +0700

    update user checkout page

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -68,6 +68,8 @@ class CheckoutController extends Controller
         $user->email = $data['email'];
         $user->name = $data['name'];
         $user->occupation = $data['occupation'];
+        $user->phone = $data['phone'];
+        $user->address = $data['address'];
         $user->save();
 
         //create checkout
